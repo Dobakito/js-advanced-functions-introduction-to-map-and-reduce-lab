b25c637fadd3426150b273b65271597a0d69b21d
Dobakito <mdobak44@icloud.com>
2021-07-20 17:55:42 -0500

    commit tree bc034ef9223f8628e1053f0fc1f69f954260d197

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+function mapToNegativize(sourceArray){
+  return sourceArray.map(i => {
+    return i * -1
+  })
+}
+
+function mapToNoChange(sourceArray){
+  return sourceArray.map(i => {
+    return  i;
+  })
+}
+
+function mapToDouble(sourceArray){
+  return sourceArray.map(i => {
+    return i * 2
+  })
+}
+
+function mapToSquare(sourceArray){
+  return sourceArray.map((i) => {
+    return i ** 2
+  })
+}
+
+
+function reduceToTotal(sourceArray, startingPoint){
+  return sourceArray.reduce((p, c) => {
+  return  p + c, startingPoint
+  })
+}
+
+reduceToAllTrue(sourceArray)
+
+reduceToAnyTrue(sourceArray)
